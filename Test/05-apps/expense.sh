80f73ff09a296e1ef76bba82122fa7b7ecf33ed8
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-05-23 22:48:43 +0530

    expense infra standard shellscript

--- Test/05-apps/expense.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dnf install ansible -y
-cd /tmp
-git clone https://github.com/rajalingarao/4.12.expense-ansible-roles-expense-terraform-dev.git
-cd 4.12.expense-ansible-roles-expense-terraform-dev
-ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
-ansible-playbook main.yaml -e component=frontend
--- Test/05-apps/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  public_subnet_id = element(split(",", data.aws_ssm_parameter.public_subnet_ids.value), 0)
-  private_subnet_id = element(split(",", data.aws_ssm_parameter.private_subnet_ids.value), 0)
-}
